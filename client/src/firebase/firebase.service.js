636dad76057c144a5fde66f080034bf8007c3ad3
milan090 <milanmuhammed1@gmail.com>
2021-02-20 18:28:11 +0530

    firebase client config using env vars

--- client/src/firebase/firebase.service.js
+++ client/src/firebase/firebase.service.js
@@ -1,7 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 
-import firebaseConfig from "./firebase.frontend.json";
+console.log(process.env.REACT_APP_API_ENDPOINT)
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
 
